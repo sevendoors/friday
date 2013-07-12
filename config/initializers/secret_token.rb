44e6e7f86251ade5bd0c83d7602b06a9121a0fdc
sevendoors <seven5272@gmail.com>
2013-07-12 00:40:42 +0000

    Start

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Friday::Application.config.secret_token = 'afe35fe39bd403fc14fb3f022f12ac0ce5871ff46ede38fe3b13ce704763a20d581135e5dc54c25bed8542202ff17b14150bb48e8b23e5f12da0222c23fd7b5f'
